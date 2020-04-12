a6b1c45bbce401a774f2183a5f21475b199aaec4
Gus <g.sosacuervo1@gmail.com>
2020-04-12 02:07:46 -0500

    Adding serilog nuget package

--- FileWatcher/FileWatcher.Service/Program.cs
+++ FileWatcher/FileWatcher.Service/Program.cs
@@ -1,23 +1,35 @@
 ﻿using System;
 using System.IO;
 using Microsoft.Extensions.Configuration;
+using Serilog;
 
 namespace FileWatcher.Service {
   class Program {
+    private static ILogger _logger;
+    private static AppConfiguration _appConfig;
+
     static void Main(string[] args) {
-      var _appConfig = getConfiguration();
+      _logger = createLogger();
+      try {
+        _appConfig = getConfiguration();
+      } catch (Exception ex) {
+        _logger.Error(ex.ToString());
+      }
+
     }
+
+    private static ILogger createLogger() => new LoggerConfiguration().WriteTo.Console().CreateLogger();
 
     private static AppConfiguration getConfiguration() {
       var configurationFilePath = Path.GetFullPath(".\\Config.json");
-      Console.WriteLine($"Config file path to search: {configurationFilePath}");
-      Console.WriteLine("starting: Parsing configuration");
+      _logger.Information(string.Format("Configuration file's path to search: {0}", configurationFilePath));
+      _logger.Information("starting: Parsing configuration");
       var configurationBuilder = new ConfigurationBuilder();
       var retval = configurationBuilder
         .AddJsonFile(configurationFilePath, false, false)
         .Build()
         .Get<AppConfiguration>();
-      Console.WriteLine("finished: Parsing configuration");
+      _logger.Information("finished: Parsing configuration");
       return retval;
     }
   }
