a996f02b64bc9d6f3fa1aa7d0cbb4a04fd7dd94e
Gus <g.sosacuervo1@gmail.com>
2020-04-12 01:57:42 -0500

    Creating solution and project with configuration file

---
+++ FileWatcher/FileWatcher.Service/AppConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace FileWatcher.Service {
+  class AppConfiguration {
+    public int SizeOfLogFileInMb { get; set; }
+    public int TimeToClearLogsInMinutes { get; set; }
+  }
+}
---
+++ FileWatcher/FileWatcher.Service/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace FileWatcher.Service {
+  class Program {
+    static void Main(string[] args) {
+      var _appConfig = getConfiguration();
+    }
+
+    private static AppConfiguration getConfiguration() {
+      var configurationFilePath = Path.GetFullPath(".\\Config.json");
+      Console.WriteLine($"Config file path to search: {configurationFilePath}");
+      Console.WriteLine("starting: Parsing configuration");
+      var configurationBuilder = new ConfigurationBuilder();
+      var retval = configurationBuilder
+        .AddJsonFile(configurationFilePath, false, false)
+        .Build()
+        .Get<AppConfiguration>();
+      Console.WriteLine("finished: Parsing configuration");
+      return retval;
+    }
+  }
+}
