e2ee9350bb041c8d8c2771888beb4bffef529d63
Gus <g.sosacuervo1@gmail.com>
2020-04-12 03:56:32 -0500

    Initializing file watcher properly

--- FileWatcher/FileWatcher.Core/FileWatcher.cs
+++ FileWatcher/FileWatcher.Core/FileWatcher.cs
@@ -1,23 +1,50 @@
 ﻿using System;
+using System.IO;
 using FileWatcher.Abstracts.Contracts;
 using FileWatcher.Abstracts.Domain;
 
 namespace FileWatcher.Core {
   public class FileWatcher : IFileWatcher {
     private FolderWatchMetadata _folder;
+    private FileSystemWatcher _watcher;
 
     public FileWatcher(FolderWatchMetadata folder) {
       this._folder = folder;
+      this._watcher = initializeWatcher();
     }
 
-    public void Dispose() => Stop();
+    private FileSystemWatcher initializeWatcher() {
+      var watcher = new FileSystemWatcher();
+      watcher.Path = _folder.FolderPath;
+      watcher.NotifyFilter = NotifyFilters.LastAccess
+                                      | NotifyFilters.LastWrite
+                                      | NotifyFilters.FileName
+                                      | NotifyFilters.DirectoryName;
+      watcher.Filter = "*.*";
+      watcher.Created += newFileCreated;
+      watcher.Deleted += newFileDeleted;
+      return watcher;
+    }
+
+    private void newFileDeleted(object sender, FileSystemEventArgs e) {
+      throw new NotImplementedException("new file deleted");
+    }
+
+    private void newFileCreated(object sender, FileSystemEventArgs e) {
+      throw new NotImplementedException("new file created");
+    }
+
+    public void Dispose() {
+      Stop();
+      _watcher.Dispose();
+    }
 
     public void Start() {
-      throw new NotImplementedException();
+      _watcher.EnableRaisingEvents = true;
     }
 
     public void Stop() {
-      throw new NotImplementedException();
+      _watcher.EnableRaisingEvents = false;
     }
   }
 }
