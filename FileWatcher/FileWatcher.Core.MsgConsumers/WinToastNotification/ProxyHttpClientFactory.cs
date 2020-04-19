c7a44dfb0776cbb2290eb4336bdad5a5459baaf1
Gus <g.sosacuervo1@gmail.com>
2020-04-19 03:10:12 -0500

    Implementing email notification

--- FileWatcher/FileWatcher.Core.MsgConsumers/WinToastNotification/ProxyHttpClientFactory.cs
+++ FileWatcher/FileWatcher.Core.MsgConsumers/WinToastNotification/ProxyHttpClientFactory.cs
@@ -1,18 +1,26 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Net.Http;
 using Flurl.Http.Configuration;
 
 namespace FileWatcher.Core.MsgConsumers.WinToastNotification {
   class ProxyHttpClientFactory : DefaultHttpClientFactory {
-    private readonly string _address;
-
-    public ProxyHttpClientFactory(string address = "http://localhost:8888") {
-      this._address = address;
+    private string _address;
+    public string Address {
+      get => _address;
+      set {
+        if (Uri.IsWellFormedUriString(value, UriKind.Absolute)) {
+          _address = value;
+        }
+      }
+    }
+    public ProxyHttpClientFactory() {
+      Address = "http://localhost:8888";
     }
 
     public override HttpMessageHandler CreateMessageHandler() {
       return new HttpClientHandler {
-        Proxy = new WebProxy(_address),
+        Proxy = new WebProxy(Address),
         UseProxy = true
       };
     }
--- FileWatcher/FileWatcher.Core.MsgConsumers/WinToastNotification/WinToastNotification.cs
+++ FileWatcher/FileWatcher.Core.MsgConsumers/WinToastNotification/WinToastNotification.cs
@@ -4,34 +4,48 @@ using System.Threading.Tasks;
 using FileWatcher.Abstracts.Contracts;
 using FileWatcher.Abstracts.Domain;
 using Flurl.Http;
+using Serilog;
 
 namespace FileWatcher.Core.MsgConsumers.WinToastNotification {
   public class WinToastNotification : IMesssageConsumer<NewFileMessage>, IMesssageConsumer<DeleteFileMessage> {
     private readonly string serverHost;
     private readonly int serverPort;
     private readonly string executableServerPath;
+    private readonly string uri;
     private Process serverProcess;
+    private ILogger logger;
 
-    public WinToastNotification(string executableServerPath, string serverHost = "localhost", int serverPort = 1234) {
+    public WinToastNotification(ILogger logger, string executableServerPath, string serverHost = "localhost", int serverPort = 1234) {
+      this.logger = logger;
+      logger.Information("starting: initializing win toast notification");
       this.serverHost = serverHost;
       this.serverPort = serverPort;
       this.executableServerPath = executableServerPath;
+      this.uri = string.Format("http://{0}:{1}", serverHost, serverPort);
+      logger.Information($"uri={uri}");
       initializeServer();
 #if DEBUG
+      var proxy = new ProxyHttpClientFactory();
       FlurlHttp.Configure(settings => {
-        settings.HttpClientFactory = new ProxyHttpClientFactory();
+        settings.HttpClientFactory = proxy;
       });
+      logger.Information($"using proxy at: {proxy.Address}");
 #endif
+      logger.Information("finished: initializing win toast notification");
     }
 
     private void initializeServer() {
+      logger.Information("starting: initializing windows server to launch the toast");
       this.serverProcess = new Process();
       serverProcess.StartInfo.FileName = executableServerPath;
       serverProcess.StartInfo.Arguments = $"{serverHost} {serverPort}";
 #if DEBUG
+      logger.Information("using shell since application is running in debug mode");
       serverProcess.StartInfo.UseShellExecute = true;
 #endif
+      logger.Information($"starting winToastNotification.Server located at {executableServerPath} with parameters \"{serverProcess.StartInfo.Arguments}\"");
       serverProcess.Start();
+      logger.Information("finished: initializing windows server to launch the toast");
     }
 
     public async Task ConsumeMessage(NewFileMessage message) =>
@@ -49,9 +63,11 @@ namespace FileWatcher.Core.MsgConsumers.WinToastNotification {
       });
 
     private async Task showToast(MessageModel messageModel) {
-      await string.Format("http://{0}:{1}", serverHost, serverPort)
-    .WithHeader("Content-Type", "application/json")
-    .PostJsonAsync(messageModel);
+      logger.Debug("starting: sending request");
+      await uri
+        .WithHeader("Content-Type", "application/json")
+        .PostJsonAsync(messageModel);
+      logger.Debug("finished: sending request");
     }
 
     public void Dispose() {
